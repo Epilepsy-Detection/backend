0ce06e846938b1c5e06567403db74cd8fcd4eecb
omaritocen <omaralaaabdelkader@gmail.com>
2022-10-29 23:51:34 +0200

    Integrate database into the application

--- startup/index.js
+++ startup/index.js
@@ -1,9 +1,11 @@
 const setupLogger = require("./logger");
 const setupRoutes = require("./routes");
 const setupConfig = require("./config");
+const setupDB = require("./db");
 
 module.exports = (app) => {
   setupLogger(app);
   setupConfig();
+  setupDB();
   setupRoutes(app);
 };
