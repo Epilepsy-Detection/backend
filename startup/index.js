5167bed9ebdb65d273ea673b6e45ba1d355123a0
omaritocen <omaralaaabdelkader@gmail.com>
2022-10-30 20:20:38 +0200

    Create auth (login, register) logic for doctors, created user and dr modles

--- startup/index.js
+++ startup/index.js
@@ -2,10 +2,12 @@ const setupLogger = require("./logger");
 const setupRoutes = require("./routes");
 const setupConfig = require("./config");
 const setupDB = require("./db");
+const setupBodyParsing = require("./body");
 
 module.exports = (app) => {
   setupLogger(app);
   setupConfig();
   setupDB();
+  setupBodyParsing(app);
   setupRoutes(app);
 };
