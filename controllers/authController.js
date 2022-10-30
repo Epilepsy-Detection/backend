19663889ab3d6c88117678f2406d2a4c34528003
omaritocen <omaralaaabdelkader@gmail.com>
2022-10-30 23:54:12 +0200

    code styling, handle empty profile error

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,6 @@ const Patient = require("../models/mongoose/patient");
 //  @access public
 //  @body   email   password
 module.exports.login = async (req, res, next) => {
-
   const user = await User.findOne({ email: req.body.email });
 
   if (!user) {
@@ -23,12 +22,15 @@ module.exports.login = async (req, res, next) => {
     return next(new AppError("Invalid Password", 400));
   }
 
-  // TODO: Get Profile depending on rule
   let profile;
   if (user.role == "doctor") {
     profile = await Doctor.findOne({ _userId: user._id });
   } else if (user.role == "patient") {
     profile = await Patient.findOne({ _userId: user._id });
+  }
+
+  if (!profile) {
+    return next(new AppError("Could not find a profile", 404));
   }
 
   userAuthenticated(res, user, profile);
@@ -39,27 +41,31 @@ module.exports.login = async (req, res, next) => {
 //  @access public
 //  @body   email password firstname lastname
 module.exports.register = async (req, res, next) => {
-
   const { email, password, firstName, lastName } = req.body;
+
   const pr = await User.findOne({ email });
   if (pr) {
     return next(new AppError("A User with the same Email Already Exists", 400));
   }
+
   const session = await mongoose.startSession();
   session.startTransaction();
+
   const user = new User({ email, password });
   await user.save({ session });
+
   const profile = new Doctor({
     _userId: user._id,
     firstName,
     lastName,
   });
   await profile.save({ session });
+
   await session.commitTransaction();
   session.endSession();
+
   userAuthenticated(res, user, profile);
 };
-
 
 // Helper function to generate Authentication token and send profile data to user
 const userAuthenticated = (res, user, profile) => {
