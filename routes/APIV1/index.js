76daf718306d81791f7ea85fade441dd3dc8e767
Muhhamed Sallam <m.sallam2622000@gmail.com>
2022-12-19 17:53:34 +0200

    added profile route

--- routes/APIV1/index.js
+++ routes/APIV1/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const auth = require("./auth");
 const patient = require("./patient");
+const profile = require("./profile");
 
 router.use("/auth", auth);
 router.use("/patient", patient);
+router.use("/profile", profile)
 
 module.exports = router;
