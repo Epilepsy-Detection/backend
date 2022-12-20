4580beb8775d335a564b34b4b7356eda9cb95971
Muhhamed Sallam <m.sallam2622000@gmail.com>
2022-12-20 04:00:13 +0200

    edited path error

--- controllers/uploadFile.js
+++ controllers/uploadFile.js
@@ -4,7 +4,7 @@ module.exports.uploadfile = async (req,res,next) => {
     const files = req.files;
 
     Object.keys(files).forEach(key =>{
-       const filepath = path.join('./',__dirname,'..', 'files', files[key].name)
+       const filepath = path.join(__dirname,'..', 'files', files[key].name)
        files[key].mv(filepath, (err) =>{
         if (err){
             return res.status(500).json({status:"error",message:err})
