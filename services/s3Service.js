be96edd9df6620f52ad0317456b0c3b7c56cd50e
Omar Alaa <omaralaaabdelkader@gmail.com>
2022-12-26 01:42:15 +0200

    pre-sign file url on creating report

--- services/s3Service.js
+++ services/s3Service.js
@@ -1,9 +1,17 @@
 const S3 = require("../instances/AWS/s3");
 
-module.exports.uploadFile = (bucketName, file, key) => {
+module.exports.uploadFile = async (bucketName, file, key) => {
   return S3.upload({
     Bucket: bucketName,
     Key: key,
     Body: file,
   }).promise();
+};
+
+module.exports.signUrl = async (bucketName, key) => {
+  return S3.getSignedUrlPromise("getObject", {
+    Bucket: bucketName,
+    Key: key,
+    Expires: 60 * 5,
+  });
 };
