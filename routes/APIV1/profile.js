c634f644e97cefb92e3c75c6d8cd67e3dd78b18e
Omar Alaa <omaralaaabdelkader@gmail.com>
2022-12-26 02:05:53 +0200

    Create get profile route for doctor patients

--- routes/APIV1/profile.js
+++ routes/APIV1/profile.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {profile} = require("../../controllers/profileController");
+const {
+  getMyProfile,
+  getPatientProfile,
+} = require("../../controllers/profileController");
 const roles = require("../../middleware/role-auth");
 
-router.route("/").get(roles(["doctor", "patient"]), profile);
+router.route("/").get(roles(["doctor", "patient"]), getMyProfile);
+
+router.route("/:profileId").get(roles(["doctor"]), getPatientProfile);
 
 module.exports = router;
