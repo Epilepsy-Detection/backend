1d7874a6613cd16c1c3472dda20504a34071a795
Omar Alaa <omaralaaabdelkader@gmail.com>
2022-12-26 02:05:53 +0200

    Create get profile route for doctor patients

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,12 +1,13 @@
 const Doctor = require("ep-det-core/models/mongoose/doctor");
 const Patient = require("ep-det-core/models/mongoose/patient");
 const AppError = require("ep-det-core/utils/AppError");
+const mongoose = require("mongoose");
 
 //  @desc   returns user profile
 //  @route  GET /api/v1/profile
 //  @access doctor - patient
 //  @Params -
-module.exports.profile = async (req, res, next) => {
+module.exports.getMyProfile = async (req, res, next) => {
   let profile;
   if (req.user.role === "doctor") {
     profile = await Doctor.findById(req.user._profileId);
@@ -19,4 +20,29 @@ module.exports.profile = async (req, res, next) => {
   }
 
   res.status(200).json({ profile });
+};
+
+//  @desc   returns a doctor's patient profile
+//  @route  GET /api/v1/profile/:profileId
+//  @access doctor
+//  @Params profileId
+module.exports.getPatientProfile = async (req, res, next) => {
+  const profileId = req.params.profileId;
+
+  // Migrate to validate params
+  if (!profileId || !mongoose.Types.ObjectId.isValid(profileId)) {
+    return next(new AppError("Invalid profileId", 400));
+  }
+
+  const patient = await Patient.findById(profileId);
+
+  if (!patient) {
+    return next(new AppError("Could not find a profile", 404));
+  } else if (patient._doctorId.toString() !== req.user._profileId) {
+    return next(
+      new AppError("You have no authorization over this patient", 403)
+    );
+  }
+
+  res.status(200).json({ patient });
 };
